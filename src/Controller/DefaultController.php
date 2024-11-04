5753f3f4cfc24771b002f67a3b18ee2f7c7d19ca
Prudence ASSOGBA <jprud67@gmail.com>
2024-11-04 15:11:11 +0100

    correction default langue

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,21 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Language;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 class DefaultController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private TranslatorInterface $translator
+    ) {}
+
     #[Route(path: '/', name: 'app_default')]
     public function defaultpage(Request $request): Response
     {
-       
+        $defaultLanguage = $this->entityManager->getRepository(Language::class)->findOneBy(["isDefault" => true])->getCode();
+
          return $this->redirectToRoute('home',[
-            '_locale' => $request->getLocale(),
+            '_locale' => $defaultLanguage,
         ],);
     }
 
